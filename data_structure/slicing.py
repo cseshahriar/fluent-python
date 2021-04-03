ffccd6522fe88d189e75e55dcf95191646d265a8
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-04-04 01:00:03 +0600

    slicing

---
+++ data_structure/slicing.py
@@ -0,0 +1,7 @@
+
+l = [10, 20, 30, 40, 50, 60]
+print(l[:2]) # index 1 item from first
+print(l[2:]) # start from 1 index 
+
+print(l[3:]) # start from 3rd index 
+print(l[:3]) # start from 3rd index
